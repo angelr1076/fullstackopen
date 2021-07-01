06df2b1ee07bef56afa78173fb35fd9d1e52d76e
Angel Rodriguez <arodriguez@ethosoutcomes.com>
2021-07-01 08:00:26 -0400

    updated phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,24 +2,46 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+      { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
-  // 2.6: The Phonebook Step1
+  const addPerson = event => {
+      event.preventDefault()
+
+      const personObject = {
+          name: newName,
+          id: newName
+      }
+
+      setPersons(persons.concat(personObject))
+      setNewName('')
+  }
+
+  const handlePersonChange = event => {
+      setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: 
+            <input 
+                value={newName}
+                onChange={handlePersonChange}  
+            />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul style={{ listStyleType: 'none' }}>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+        
     </div>
   )
 }
