463fa4438599979a4b88659b6acaef14e9e3ecf7
Angel Rodriguez <angelr1076@gmail.com>
2021-07-23 18:17:45 -0400

    Refactored project 4 folder structure for 4.1 and 4.2 and added controllers models and utils folder with app file

--- part4/index.js
+++ part4/index.js
@@ -1,45 +1,57 @@
+const app = require('./app'); // the actual Express application
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const logger = require('./utils/logger');
 
-const url = process.env.MONGODB_URI;
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// });
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+// const Blog = mongoose.model('Blog', blogSchema);
 
-app.use(cors());
-app.use(express.json());
+// const url = process.env.MONGODB_URI;
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
+// app.use(cors());
+// app.use(express.json());
 
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then(blogs => {
+//     response.json(blogs);
+//   });
+// });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body);
+
+//   blog.save().then(result => {
+//     response.status(201).json(result);
+//   });
+// });
+
+// const PORT = 3003;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
---
+++ part4/models/blog.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
+  author: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  url: {
+    type: String,
+    minLength: 5,
+  },
+  likes: {
+    type: Number,
+  },
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
