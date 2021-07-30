c16d1af8bbfda54cd6cdd99f963a7aaa2f72072e
Angel Rodriguez <arodriguez@ethosoutcomes.com>
2021-07-30 07:25:05 -0400

    Added favoriteBlog exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,21 +1,23 @@
 const dummy = blogs => {
-  blogs = 1;
-  return blogs;
+    blogs = 1;
+    return blogs;
 };
 
 const totalLikes = blogs => {
-  return blogs.reduce((sum, blogs) => sum + blogs.likes, 0);
+    return blogs.reduce((sum, blogs) => sum + blogs.likes, 0);
 };
 
 const favoriteBlog = blogs => {
-  return blogs.reduce((max, obj) => {
-    console.log('max likes = ', max);
-    // obj.likes > max.likes ? obj : max;
-  });
+    return blogs.length === 0 ?
+        {} :
+        blogs.reduce(
+            (previous, next) => (next.likes > previous ? next.likes : previous),
+            blogs[0].likes,
+        );
 };
 
 module.exports = {
-  dummy,
-  totalLikes,
-  favoriteBlog,
+    dummy,
+    totalLikes,
+    favoriteBlog,
 };
