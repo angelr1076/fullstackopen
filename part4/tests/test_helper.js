ad5cf2c2152c1e2f68cda9cb26b2504cd2225ae8
Angel Rodriguez <angelr1076@gmail.com>
2021-08-09 07:44:55 -0400

    Added test_helper and updated 4.9

---
+++ part4/tests/test_helper.js
@@ -0,0 +1,19 @@
+const initialBlogs = [{
+        title: 'My first blog',
+        author: 'Angel Rodriguez',
+        url: 'http://localhost3003/api/blogs',
+        likes: 0,
+        id: '60fb343d2eee5352db9429c6',
+    },
+    {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://www.cs.utexas.edu/users/EWD/indexBibTeX.html',
+        likes: 12,
+        id: '60ff5d5baf4d677086ba444a',
+    },
+];
+
+module.exports = {
+    initialBlogs,
+};
