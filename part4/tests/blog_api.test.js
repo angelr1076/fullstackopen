5a516b645001481c25a7db3cc3e698e2b529bb6c
Angel Rodriguez <angelr1076@gmail.com>
2021-08-10 20:42:26 -0400

    completed 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const blog = require('../models/blog');
 const api = supertest(app);
 const Blog = require('../models/blog');
 const helper = require('./test_helper');
@@ -8,11 +9,9 @@ const helper = require('./test_helper');
 beforeEach(async() => {
     await Blog.deleteMany({});
 
-    let blogObject = new Blog(helper.initialBlogs[0]);
-    await blogObject.save();
-
-    blogObject = new Blog(helper.initialBlogs[1]);
-    await blogObject.save();
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+    const promiseArray = blogObjects.map(blog => blog.save());
+    await Promise.all(promiseArray);
 });
 
 test('blogs are returned as json', async() => {
