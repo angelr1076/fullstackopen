7f41b976d23a06be0b008016105d8fd9c444c528
Angel Rodriguez <angelr1076@gmail.com>
2021-08-10 21:07:44 -0400

    Added test for exercise 4.11

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,27 +1,34 @@
 const Blog = require('../models/blog');
 
-const initialBlogs = [{
-        title: 'My first blog',
-        author: 'Angel Rodriguez',
-        url: 'http://localhost3003/api/blogs',
-        likes: 0,
-        id: '60fb343d2eee5352db9429c6',
-    },
-    {
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://www.cs.utexas.edu/users/EWD/indexBibTeX.html',
-        likes: 12,
-        id: '60ff5d5baf4d677086ba444a',`
-    },
+const initialBlogs = [
+  {
+    title: 'My first blog',
+    author: 'Angel Rodriguez',
+    url: 'http://localhost3003/api/blogs',
+    likes: 0,
+    id: '60fb343d2eee5352db9429c6',
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://www.cs.utexas.edu/users/EWD/indexBibTeX.html',
+    likes: 12,
+    id: '60ff5d5baf4d677086ba444a',
+  },
 ];
 
-const blogsInDb = async() => {
-    const blogs = await Blog.find({});
-    return blogs.map(blog => blog.toJSON());
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+};
+
+const likesEmpty = async () => {
+  const blogs = await Blog.find({});
+  return blogs.filter(blog => console.log(blog.likes === 0));
 };
 
 module.exports = {
-    initialBlogs,
-    blogsInDb,
+  initialBlogs,
+  blogsInDb,
+  likesEmpty,
 };
