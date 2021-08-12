43d586f45e86a279b32ba79cf27a8602866c28cb
Angel Rodriguez <angelr1076@gmail.com>
2021-08-12 08:36:21 -0400

    Updated schema to set likes default to 0

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,31 +1,32 @@
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    minLength: 5,
-  },
-  author: {
-    type: String,
-    required: true,
-    minLength: 3,
-  },
-  url: {
-    type: String,
-    minLength: 5,
-  },
-  likes: {
-    type: Number,
-  },
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+    },
+    author: {
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    url: {
+        type: String,
+        minLength: 5,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
 });
 
 blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
