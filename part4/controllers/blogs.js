f87bcf8928a7d4d196a831c5b4e1f877a2787045
Angel Rodriguez <angelr1076@gmail.com>
2021-08-13 23:22:01 -0400

    Added exercise 4.13

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,24 +1,24 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', async(request, response) => {
-    const blogs = await Blog.find({});
-    response.json(blogs);
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
 blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body);
+  const blog = new Blog(request.body);
 
-    if (blog.title === undefined || blog.url === undefined) {
-        return response.status(400).end();
-    }
+  if (blog.title === undefined || blog.url === undefined) {
+    return response.status(400).end();
+  }
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        })
-        .catch(error => next(error));
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    })
+    .catch(error => next(error));
 });
 
 module.exports = blogsRouter;
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -4,91 +4,95 @@ const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
 const helper = require('./test_helper');
-jest.useFakeTimers();
+// jest.useFakeTimers();
 
-beforeEach(async() => {
-    await Blog.deleteMany({});
+beforeEach(async () => {
+  await Blog.deleteMany({});
 
-    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
-    const promiseArray = blogObjects.map(blog => blog.save());
-    await Promise.all(promiseArray);
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+  const promiseArray = blogObjects.map(blog => blog.save());
+  await Promise.all(promiseArray);
 });
 
-test('blogs are returned as json', async() => {
+describe('return blog entries attributes', () => {
+  test('blogs are returned as json', async () => {
     await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/);
-});
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('there are two blogs', async() => {
+  test('there are two blogs', async () => {
     const response = await api.get('/api/blogs');
     expect(response.body).toHaveLength(2);
-});
+  });
 
-test('the first note is about my first blog', async() => {
+  test('the first entry is about string reduction', async () => {
     const blogs = await Blog.find({});
-    expect(blogs[0].title).toBe('My first blog');
-});
+    expect(blogs[0].title).toBe('Canonical string reduction');
+  });
 
-test('unique identifier is id', async() => {
+  test('unique identifier is id', async () => {
     const blogs = await Blog.find({});
     expect(blogs[0]._id).toBeDefined();
+  });
 });
 
-test('a blog post can be created', async() => {
+describe('when a blog entry is created', () => {
+  test('a blog post can be created', async () => {
     const newBlog = {
-        title: 'hegemony is the key to leadership',
-        author: 'Anonymous',
-        url: 'https://www.google.com/sample',
-        likes: 4,
+      title: 'hegemony is the key to leadership',
+      author: 'Anonymous',
+      url: 'https://www.google.com/sample',
+      likes: 4,
     };
 
     await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/);
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
     const allBlogs = await helper.blogsInDb();
     expect(allBlogs).toHaveLength(helper.initialBlogs.length + 1);
     const titles = allBlogs.map(blog => blog.title);
     expect(titles).toContainEqual('hegemony is the key to leadership');
-});
+  });
 
-test('default likes to 0 if likes property is empty', async() => {
+  test('default likes to 0 if likes property is empty', async () => {
     const newBlog = {
-        title: 'This is an object',
-        author: 'Tazer',
-        url: 'https://www.netlify.com/sample',
+      title: 'This is an object',
+      author: 'Tazer',
+      url: 'https://www.netlify.com/sample',
     };
 
     await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/);
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
     const blogPosts = await helper.blogsInDb();
     const findBlogPost = await blogPosts.find(
-        blog => blog.title === 'This is an object'
+      blog => blog.title === 'This is an object',
     );
     console.log('blog posted: ', findBlogPost);
     const likes = findBlogPost.likes;
     expect(likes).toBe(0);
-});
+  });
 
-test('if the title and url are missing respond with status code 400', async() => {
+  test('if the title and url are missing respond with status code 400', async () => {
     const newBlog = {
-        author: 'Not Me',
-        likes: 5,
+      author: 'Not Me',
+      likes: 5,
     };
 
     await api.post('/api/blogs').send(newBlog).expect(400);
     const allBlogs = await helper.blogsInDb();
     expect(allBlogs).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
 afterAll(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
