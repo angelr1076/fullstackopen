a401c0522c8185a0c3de6bfb47c2a57bec3688fb
Angel Rodriguez <angelr1076@gmail.com>
2021-12-13 08:21:23 -0500

    completed 6.4 adding anecdotes

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { upvoteAnecdote } from './reducers/anecdoteReducer';
+import NewAnecdote from './components/NewAnecdote';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state);
+  const dispatch = useDispatch();
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+  const vote = id => {
+    console.log('vote', id);
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => dispatch(upvoteAnecdote(anecdote.id))}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
+      <NewAnecdote />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
