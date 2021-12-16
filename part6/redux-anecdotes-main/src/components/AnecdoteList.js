f2a8f3f366b0884175fa1122291a03ee14747f86
Angel Rodriguez <angelr1076@gmail.com>
2021-12-16 15:53:06 -0500

    completed 6.9 thru 6.10

--- part6/redux-anecdotes-main/src/components/AnecdoteList.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.js
@@ -2,39 +2,31 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { upvoteAnecdote } from '../reducers/anecdoteReducer';
 
-
-
 const Upvote = ({ anecdote, handleClick }) => {
-  return(
+  return (
     <div>
-    {anecdote.content}
-    <br/>
-    {' '}
-    has {anecdote.votes} votes {' '}
-    <button onClick={handleClick}>
-    vote
-    </button> 
-    <hr/>
+      {anecdote.content}
+      <br /> has {anecdote.votes} votes{' '}
+      <button onClick={handleClick}>vote</button>
+      <hr />
     </div>
-  )
-}
+  );
+};
 const Anecdotes = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(state => state.content);
   const dispatch = useDispatch();
-  
+
   return (
     <ul>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <Upvote
           key={anecdote.id}
           anecdote={anecdote}
-          handleClick={() => 
-            dispatch(upvoteAnecdote(anecdote.id))
-          }
+          handleClick={() => dispatch(upvoteAnecdote(anecdote.id))}
         />
-      )}
+      ))}
     </ul>
-  )
+  );
 };
 
 export default Anecdotes;
