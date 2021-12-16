da456cc9f40dfe1f289a969204b25119035a6d16
Angel Rodriguez <angelr1076@gmail.com>
2021-12-16 08:01:44 -0500

    Cleaned up App.js

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { upvoteAnecdote } from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  
-
-  const vote = id => {
-    console.log('vote', id);
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
