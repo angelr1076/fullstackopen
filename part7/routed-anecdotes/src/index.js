ab5f590bc1ce325ebe7d7dbf75da207a0a095924
Angel Rodriguez <angelrodriguez@Angels-Air.fios-router.home>
2022-04-15 14:02:10 -0400

    Updated index.js

--- part7/routed-anecdotes/src/index.js
+++ part7/routed-anecdotes/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <App />
+  </Router>,
+);
